02e823e1a77c6d39f99ee23c141daee7cad76195
Pol420 <pfernandezgu@edu.tecnocampus.cat>
2021-04-21 00:03:32 +0200

    health, shield and status HUD
    H para Herir
    V para curar Vida
    E para regenerar Escudo

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
+    [Header("Ammo")]
     [SerializeField] private Text fireText = null;
     [SerializeField] private Text waterText = null;
     [SerializeField] private Text teslaText = null;
     [SerializeField] private Image selector = null;
-    
+
+    [Header("Status")]
+    [SerializeField] private Image healthBar = null;
+    [SerializeField] private Image shieldBar = null;
+
     public void SetFire(int amount) { fireText.text = amount + ""; }
     public void SetWater(int amount) { waterText.text = amount + ""; }
     public void SetTesla(int amount) { teslaText.text = amount + ""; }
@@ -20,4 +25,7 @@ public class HUD : MonoBehaviour
         else if (pos == 1) selector.transform.position = waterText.transform.position;
         else selector.transform.position = teslaText.transform.position;
     }
+
+    public void SetHealth(float proportion) { healthBar.fillAmount = proportion; }
+    public void SetShield(float proportion) { shieldBar.fillAmount = proportion; }
 }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,23 +6,32 @@ public class PlayerStats : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Vector3Int ammo = new Vector3Int(99, 99, 99);
-    [SerializeField] private HUD ammoHud = null;
+    [SerializeField] private HUD hud = null;
     private static PlayerStats instance;
+
+    [Header("Status")]
+    [SerializeField] [Range(0f,500f)] private float maxHealth = 300f;
+    [SerializeField] [Range(0f,500f)] private float maxShield = 300f;
+    [SerializeField] [Range(0f, 1f)] private float shieldAbsorption = 0.75f;
+    private float currentHealth;
+    private float currentShield;
 
     private void Awake()
     {
         if (instance == null) instance = this;
-
     }
 
     void Start()
     {
-        
+        SetHealth(maxHealth);
+        SetShield(maxShield);
     }
     
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.H)) Hurt(5f);
+        if (Input.GetKey(KeyCode.V)) AddHealth(5f);
+        if (Input.GetKey(KeyCode.E)) AddShield(1f);
     }
 
     public static PlayerStats Instance() { return instance; }
@@ -41,12 +50,36 @@ public class PlayerStats : MonoBehaviour
     public void AddFireAmmo(int amount) { SetFireAmmo(ammo.x + amount); }
     public void AddWaterAmmo(int amount) { SetWaterAmmo(ammo.y + amount); }
     public void AddTeslaAmmo(int amount) { SetTeslaAmmo(ammo.z + amount); }
-    public void SetFireAmmo(int amount) { ammo.x = Mathf.Clamp(amount, 0, 100); ammoHud.SetFire(ammo.x); }
-    public void SetWaterAmmo(int amount) { ammo.y = Mathf.Clamp(amount, 0, 100); ammoHud.SetWater(ammo.y); }
-    public void SetTeslaAmmo(int amount) { ammo.z = Mathf.Clamp(amount, 0, 100); ammoHud.SetTesla(ammo.z); }
+    public void SetFireAmmo(int amount) { ammo.x = Mathf.Clamp(amount, 0, 100); hud.SetFire(ammo.x); }
+    public void SetWaterAmmo(int amount) { ammo.y = Mathf.Clamp(amount, 0, 100); hud.SetWater(ammo.y); }
+    public void SetTeslaAmmo(int amount) { ammo.z = Mathf.Clamp(amount, 0, 100); hud.SetTesla(ammo.z); }
     public int GetFireAmmo() { return ammo.x; }
     public int GetWaterAmmo() { return ammo.y; }
     public int GetTeslaAmmo() { return ammo.z; }
-    public void SetHudSelector(float rate) { ammoHud.SetSelector(rate); }
-    public void MoveHudSelector(int slot) { ammoHud.MoveSelector(slot); }
+    public void SetHudSelector(float rate) { hud.SetSelector(rate); }
+    public void MoveHudSelector(int slot) { hud.MoveSelector(slot); }
+
+    private void Hurt(float amount)
+    {
+        if (currentShield > 0f)
+        {
+            AddShield(-amount * shieldAbsorption);
+            AddHealth(-amount * (1f - shieldAbsorption));
+        }
+        else AddHealth(-amount);
+    }
+
+    public void AddHealth(float amount) { SetHealth(Mathf.Clamp(currentHealth + amount, 0f, maxHealth)); }
+    public void SetHealth(float amount)
+    {
+        currentHealth = amount;
+        hud.SetHealth(currentHealth / maxHealth);
+    }
+    public void AddShield(float amount) { SetShield(Mathf.Clamp(currentShield + amount, 0f, maxShield)); }
+    public void SetShield(float amount)
+    {
+        currentShield = amount;
+        hud.SetShield(currentShield / maxShield);
+    }
+
 }
