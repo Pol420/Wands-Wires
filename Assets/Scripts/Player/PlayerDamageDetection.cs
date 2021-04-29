e142da81b5c1a9ebc2b82641b28ae50192d54f5a
alcoru <55741940+alcoru@users.noreply.github.com>
2021-04-29 21:34:20 +0200

    Balas eliminadas al chocar

--- Assets/Scripts/Player/PlayerDamageDetection.cs
+++ Assets/Scripts/Player/PlayerDamageDetection.cs
@@ -12,6 +12,15 @@ public class PlayerDamageDetection : MonoBehaviour
         playerStats = PlayerStats.Instance();
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("EnemyProjectile")) 
+            playerStats.Hurt(other.transform.GetComponent<Enemy_Projectile>().GetDamage());
+        else if (other.transform.CompareTag("EnemyAttack")) 
+            playerStats.Hurt(other.transform.parent.GetComponent<Enemy_Data>().GetAttackDamage());
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.transform.CompareTag("EnemyProjectile")) 
